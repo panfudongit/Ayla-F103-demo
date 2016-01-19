a0f499b3855c19c425538a4c41304f923d5569ea
panfudongit <116865376@qq.com>
2016-01-19 17:09:30 +0800

    add demo source

---
+++ arch/mcu_io.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2011-2012 Ayla Networks, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Ayla Networks, Inc.
+ */
+#ifndef __AYLA_MCU_IO_H__
+#define __AYLA_MCU_IO_H__
+
+#define CPU_CLK_HZ 24000000
+#define SYSTICK_HZ 100
+#define BUTTON_ON_MIN_TICKS  (2 * SYSTICK_HZ)   /* button debounce time */
+#define BUTTON_DEBOUNCE_TICKS (20 * SYSTICK_HZ / 1000) /* minimum of 10ms */
+
+#define LED0_PIN	8		/* Blue LED */
+#define LED1_PIN	15	/* Green LED */
+#define LED_GPIO	GPIOE
+
+#define BUTTON_GPIO	GPIOA
+#define BUTTON_PIN	0
+#define BUTTON_EXT_LINE	EXTI_Line0
+#define BUTTON_IRQ	EXTI0_IRQn
+#define BUTTON_PORT_SOURCE GPIO_PortSourceGPIOA
+#define BUTTON_PIN_SOURCE GPIO_PinSource0
+
+#define BUTTON1_GPIO	GPIOC
+#define BUTTON1_PIN	0
+
+#define READY_N_GPIO	GPIOA
+#define READY_N_PIN		   3
+
+#define RESET_N_GPIO	GPIOA
+#define RESET_N_PIN			2
+
+#define WKUP_GPIO       GPIOA
+#define WKUP_PIN        	1
+
+/*
+#define LINK_N_GPIO		GPIOB
+#define LINK_N_PIN			1
+*/
+
+struct intr_stats {
+	u32 button;
+	u32 intr_line;
+};
+
+extern struct intr_stats intr_stats;
+extern volatile u32 tick;
+
+void mcu_io_init(void);
+
+/*
+ * Return mask for bit number.
+ */
+static inline u32 bit(u32 i)
+{
+	return 1U << i;
+}
+
+#endif /* __AYLA_MCU_IO_H__ */
---
+++ example/ledevb/demo.c
@@ -0,0 +1,345 @@
+/*
+ * Copyright 2011-2013 Ayla Networks, Inc.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are
+ * those of the authors and should not be interpreted as representing official
+ * policies, either expressed or implied, of Ayla Networks, Inc.
+ */
+
+#include <string.h>
+#include <ayla/mcu_platform.h>
+#include <mcu_io.h>
+#include <stm32.h>
+
+#ifdef DEMO_CONF
+#include <ayla/conf_token.h>
+#include <ayla/conf_access.h>
+#endif
+
+#include <ayla/ayla_proto_mcu.h>
+#include <ayla/props.h>
+#include <ayla/serial_msg.h>
+
+
+
+#ifdef DEMO_UART
+#include <ayla/uart.h>
+#else
+#include <spi_platform_arch.h>
+#endif /* DEMO_UART */
+
+#ifdef DEMO_FILE_PROP
+#include <ayla/prop_dp.h>
+#include <demo_stream.h>
+#endif /* DEMO_FILE_PROP */
+
+#ifdef DEMO_FILE_PROP
+#define VERSION "demo_dp 1.3"
+#else
+#define VERSION "demo 1.3"
+#endif /* DEMO_FILE_PROP */
+
+#ifdef DEMO_SCHED_LIB
+#include <ayla/schedeval.h>
+#include <ayla/sched.h>
+#include <ayla/cmp.h>
+#endif /* DEMO_SCHED_LIB */
+
+
+#ifdef DEMO_IMG_MGMT
+#include <flash_layout.h>
+#define BUILD_DATE ""
+#ifdef AYLA_KEIL
+/*
+ * Image header location is fixed.
+ */
+#define IMG_HDR_LOC		(MCU_IMG_ACTIVE + IMAGE_HDR_OFF)
+#define IMG_HDR_VER_LOC		(IMG_HDR_LOC + sizeof(struct image_hdr))
+
+const struct image_hdr __img_hdr
+			__attribute__((used))
+			__attribute((at(IMG_HDR_LOC)));
+const char version[72] __attribute((at(IMG_HDR_VER_LOC))) =
+	 VERSION " " BUILD_DATE;
+#else
+const char version[] __attribute__((section(".version"))) =
+	VERSION " " BUILD_DATE;
+#endif /* AYLA_KEIL */
+#else
+const char version[] = VERSION;
+#endif /* DEMO_IMG_MGMT || AYLA_BUILD_VERSION */
+
+static u8 factory_reset;
+
+
+static void set_input(struct prop *, void *arg, void *valp, size_t len);
+static void set_cmd(struct prop *, void *arg, void *valp, size_t len);
+static void set_dec_in(struct prop *prop, void *arg, void *valp, size_t len);
+
+#ifdef DEMO_IMG_MGMT
+extern u8 boot2inactive;
+extern u8 template_req;
+void mcu_img_mgmt_init(void);
+int send_inactive_version(struct prop *, void *arg);
+void set_boot2inactive(struct prop *, void *arg, void *valp, size_t len);
+int send_template_version(struct prop *, void *arg);
+void template_version_sent(void);
+#endif
+
+static s32 input;
+static s32 output;
+static s32 decimal_in;
+static s32 decimal_out;
+static u8 blue_button;
+static char cmd_buf[TLV_MAX_STR_LEN + 1];
+
+#ifdef DEMO_SCHED_LIB
+static u8 sched_buf[256];
+static u8 sched_out_length;
+static const u8 *sched_out;
+static struct sched_prop schedule_in;
+static void set_schedule_in(struct prop *prop, void *arg, void *valp,
+    size_t len);
+#endif
+
+
+static void set_led(struct prop *prop, void *arg, void *valp, size_t len)
+{
+	u8 val = *(u8 *)valp;
+	u32 pin = (u32)arg;
+
+	stm32_set_led(pin, val);
+}
+
+static int send_led(struct prop *prop, void *arg)
+{
+	u8 val;
+	u32 pin = (u32)prop->arg;
+
+	val = stm32_get_led(pin);
+	return prop_send(prop, &val, sizeof(val), arg);
+}
+
+static int send_version(struct prop *prop, void *arg)
+{
+	return prop_send(prop, version, strlen(version), arg);
+}
+
+#ifdef DEMO_SCHED_LIB
+static int send_schedule(struct prop *prop, void *arg)
+{
+	return prop_send(prop, sched_out, sched_out_length, arg);
+}
+#endif
+
+
+struct prop prop_table[] = {
+	{ "Blue_button", ATLV_BOOL, NULL, prop_send_generic,
+	    &blue_button, sizeof(blue_button), AFMT_READ_ONLY},
+#define PROP_BUTTON 0
+	{ "output", ATLV_INT, NULL, prop_send_generic, &output,
+	    sizeof(output), AFMT_READ_ONLY},
+#define PROP_OUTPUT 1
+	{ "log", ATLV_UTF8, NULL, prop_send_generic, &cmd_buf[0],
+	    0, AFMT_READ_ONLY},
+#define PROP_LOG 2
+	{ "decimal_out", ATLV_CENTS, NULL, prop_send_generic, &decimal_out,
+	    sizeof(decimal_out), AFMT_READ_ONLY},
+#define PROP_DEC_OUT 3
+	{ "decimal_in", ATLV_CENTS, set_dec_in, prop_send_generic,
+	    &decimal_in, sizeof(decimal_in)},
+	{ "Blue_LED", ATLV_BOOL, set_led, send_led,
+	    (void *)(1 << LED0_PIN), 1},
+	{ "Green_LED", ATLV_BOOL, set_led, send_led,
+	    (void *)(1 << LED1_PIN), 1},
+#ifdef DEMO_SCHED_LIB
+	{ "schedule_in", ATLV_SCHED, set_schedule_in, NULL, &schedule_in},
+	{ "schedule_out", ATLV_SCHED, NULL, send_schedule, NULL, 0,
+	  AFMT_READ_ONLY},
+#define PROP_SCHED_OUT 8
+#endif
+	{ "cmd", ATLV_UTF8, set_cmd, prop_send_generic, &cmd_buf[0]},
+	{ "input", ATLV_INT, set_input, prop_send_generic,
+	    &input, sizeof(input)},
+	{ "version", ATLV_UTF8, NULL, send_version, NULL, 0, AFMT_READ_ONLY},
+#ifdef DEMO_IMG_MGMT
+	{ "inactive_version", ATLV_UTF8, NULL, send_inactive_version, NULL,
+	  0, AFMT_READ_ONLY },
+	{ "boot_to_inactive", ATLV_BOOL, set_boot2inactive, prop_send_generic,
+	  &boot2inactive, sizeof(boot2inactive) },
+	{ "oem_host_version", ATLV_UTF8, NULL, send_template_version },
+#endif
+#ifdef DEMO_FILE_PROP
+	/*
+	 * Long data points must use property type ATLV_LOC in this table,
+	 * even though they have type ATLV_BIN in the protocol.
+	 */
+	{ "stream_up_len", ATLV_INT, set_length_up, prop_send_generic,
+	    &stream_up_len, sizeof(stream_up_len)},
+	{ "stream_up", ATLV_LOC, NULL, prop_dp_send, &stream_up_state, 0},
+	{ "stream_down", ATLV_LOC, prop_dp_set, prop_dp_send,
+	    &stream_down_state, 0},
+	{ "stream_down_len", ATLV_UINT, NULL, prop_send_generic,
+	   &stream_down_state.next_off, sizeof(stream_down_state.next_off)},
+	{ "stream_down_match_len", ATLV_UINT, NULL, prop_send_generic,
+	   &stream_down_patt_match_len, sizeof(stream_down_patt_match_len)},
+#endif /* DEMO_FILE_PROP */
+	{ NULL }
+};
+u8 prop_count = (sizeof(prop_table) / sizeof(prop_table[0])) - 1;
+
+static void set_input(struct prop *prop, void *arg, void *valp, size_t len)
+{
+	s32 i = *(s32 *)valp;
+
+	if (len != sizeof(s32)) {
+		return;
+	}
+	input = i;
+	if (i > 0x7fff || i < -0x8000) {
+		output = -1;		/* square would overflow */
+	} else {
+		output = i * i;
+	}
+	prop_table[PROP_OUTPUT].send_mask = valid_dest_mask;
+}
+
+static void set_dec_in(struct prop *prop, void *arg, void *valp, size_t len)
+{
+	s32 i = *(s32 *)valp;
+
+	if (len != sizeof(s32)) {
+		return;
+	}
+	decimal_in = i;
+	decimal_out = i;
+	prop_table[PROP_DEC_OUT].send_mask = valid_dest_mask;
+}
+
+static void set_cmd(struct prop *prop, void *arg, void *valp, size_t len)
+{
+	if (len >= sizeof(cmd_buf)) {
+		len = sizeof(cmd_buf) - 1;
+	}
+	memcpy(cmd_buf, valp, len);
+	cmd_buf[len] = '\0';
+	prop_table[PROP_LOG].send_mask = valid_dest_mask;
+}
+
+#ifdef DEMO_SCHED_LIB
+static void set_schedule_in(struct prop *prop, void *arg, void *valp,
+    size_t len)
+{
+	if (len > sizeof(sched_buf)) {
+		len = sizeof(sched_buf);
+	}
+	memcpy(sched_buf, valp, len);
+	sched_out = sched_buf;
+	sched_out_length = len;
+	prop_table[PROP_SCHED_OUT].send_mask = valid_dest_mask;
+	memcpy(schedule_in.tlvs, valp, len);
+	schedule_in.len = sched_out_length;
+	sched_run_all(NULL);
+}
+#endif
+
+/*
+ * Blue button push observed by interrupt handler.
+ * Callers are in stm32.c
+ */
+void demo_set_button_state(u8 button_value)
+{
+	blue_button = button_value;
+	prop_table[PROP_BUTTON].send_mask = valid_dest_mask;
+}
+
+int main(int argc, char **argv)
+{
+	struct prop *prop;
+
+	feature_mask |= MCU_LAN_SUPPORT;
+#ifdef DEMO_IMG_MGMT
+	mcu_img_mgmt_init();
+	feature_mask |= MCU_OTA_SUPPORT;
+#endif
+#ifdef DEMO_SCHED_LIB
+	feature_mask |= MCU_TIME_SUBSCRIPTION;
+#endif
+	mcu_io_init();
+#ifdef DEMO_UART
+	feature_mask |= MCU_DATAPOINT_CONFIRM;
+	uart_init();
+#else
+	spi_platform_init();
+#endif
+	stm32_reset_module();
+	stm32_init();
+	//factory_reset = stm32_factory_reset_detect();
+#ifdef DEMO_FILE_PROP
+	demo_stream_init();
+#endif /* DEMO_FILE_PROP */
+	for (;;) {
+		if (stm32_ready()) {
+			if (factory_reset &&
+			    !serial_tx_cmd(ACMD_LOAD_FACTORY, NULL, 0)) {
+				factory_reset = 0;
+				//stm32_set_factory_rst_led(0);
+				while (stm32_ready()) {
+					serial_poll();
+				}
+			}
+#ifdef DEMO_CONF
+			conf_poll();
+#endif
+			prop_poll();
+			serial_poll();
+#ifdef DEMO_SCHED_LIB
+			if (sched_next_event_tick &&
+			    (tick == sched_next_event_tick ||
+			    cmp_gt(tick, sched_next_event_tick))) {
+				sched_run_all(&sched_next_event_tick);
+			}
+#endif
+		}
+#ifdef DEMO_IMG_MGMT
+		if (template_req &&
+		    prop_send_done(prop_lookup("oem_host_version")) == 0) {
+			/*
+			 * Template version number has been sent.
+			 */
+		//	template_version_sent();
+		}
+#endif
+		prop = prop_lookup_error();
+		if (prop != NULL) {
+			/*
+			 * Property send has failed with error code.
+			 * Error code is available in prop->send_err
+			 *
+			 * Insert logic here to handle the failure.
+			 */
+			prop->send_err = 0;
+		}
+	}
+}
